eeae073beef3d236572ce43fd64c1f628e7223e5
Victor Cruz <adsi.itam.26@gmail.com>
2018-04-20 10:23:53 -0500

    Actualizacion de UT, con atraso de red

--- examen/src/test/java/mx/com/itam/adsi/examen/SolutionTest.java
+++ examen/src/test/java/mx/com/itam/adsi/examen/SolutionTest.java
@@ -31,7 +31,26 @@ public class SolutionTest {
 		Solution s = new Solution();
 		assertTrue("Con abab: ", s.problema("abab"));
 		assertTrue("Con abab: ", !s.problema("aba"));
-		assertTrue("Con abab: ", s.problema("gustavogustavogustavo"));
+		assertTrue("Con abab: ", s.problema("problematavoproblematavoproblematavo"));
+		assertTrue("Prueba para abab", s.problema("abab"));
+		assertTrue("Prueba para ababa", !s.problema("ababa"));
+		assertTrue("Prueba para victorvictor", s.problema("victorvictor"));
+		assertTrue("Prueba para victorvicto", !s.problema("victorvicto"));
+		assertTrue("Prueba para dandan", s.problema("dandan"));
+		assertTrue("Prueba para danda", !s.problema("danda"));
+		assertTrue("Prueba para abaab", !s.problema("abaab"));
+		assertTrue("Prueba para abad", !s.problema("abad"));
+		assertTrue("Prueba para ababc", !s.problema("ababc"));
+		assertTrue("Prueba para cabcaabc", !s.problema("cabcaabc"));
+		assertTrue("Prueba para baccab", !s.problema("baccab"));
+		assertTrue("Prueba para bacbac", s.problema("bacbac"));
+		assertTrue("Prueba para bacacb", !s.problema("bacacb"));
+		assertTrue("Prueba para 123321", !s.problema("123321"));
+		assertTrue("Prueba para 123123", s.problema("123123"));
+		assertTrue("Prueba para 1214", !s.problema("1214"));
+		assertTrue("Prueba para holaola", !s.problema("olaHola"));
+		assertTrue("Prueba para holahola", s.problema("holahola"));
+		assertTrue("Prueba para holaholas", !s.problema("holaholas"));
 	}
 	
 
